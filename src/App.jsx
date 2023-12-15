a593d105356122c07a859279cbd46715626a2731
Farid Rashidov <farikrashidov@gmail.com>
2023-12-16 01:35:57 +0400

    Create: Home page and adding Hero component and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./pages/Navbar.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import background from "../assets/background.png";
+import heroCar from "../assets/home-car.png";
+import { FaRegCheckCircle } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
+const Hero = () => {
+  const iconStyle = { color: "red" };
+  return (
+    <section id="hero" className="hero-section">
+      <div className="hero-container">
+        <img src={background} alt="Background" className="hero-background" />
+        <div className="hero-content">
+          <div className="hero-content-text">
+            <h1>Plan your trip now</h1>
+            <p className="hero-content-subtitle">
+              Save <span>big</span> with our
+              <br />
+              car rental
+            </p>
+            <p className="hero-content-description">
+              Rent the car of your dreams. Unbeatable prices, unlimited miles,
+              <br />
+              flexible pick-up options and much more.
+            </p>
+            <div className="hero-content-btn">
+              <a href="/" className="hero-content-btn-booking">
+                Book Ride <FaRegCheckCircle className="icon-btn" />
+              </a>
+              <a href="/" className="hero-content-btn-learn-more">
+                Learn More <FaChevronRight className="icon-btn" />
+              </a>
+            </div>
+          </div>
+          <img src={heroCar} alt="Car Photo" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
