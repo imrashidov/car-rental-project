f3a492ff3645b82462401d3bfcf007f891cca8b4
imrashidov <farikrashidov@gmail.com>
2024-03-10 07:52:45 +0400

    Add: Banner section component and styles

---
+++ src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <section id="banner" className="banner-section">
+      <div>
+        <h2>Save big with our cheap car rental!</h2>
+        <p>
+          Top Airports. Local Suppliers. <span>24/7</span> Support.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/Hero.jsx";
 import Book from "../components/Book.jsx";
 import PlanTrip from "../components/PlanTrip.jsx";
 import PickCar from "../components/PickCar.jsx";
+import Banner from "../components/Banner.jsx";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Book />
       <PlanTrip />
       <PickCar />
+      <Banner />
     </div>
   );
 };
