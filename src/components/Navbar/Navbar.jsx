9fc0293ae4be04e9aa6cb019f75a793cdd096055
imrashidov <farikrashidov@gmail.com>
2024-03-21 02:40:30 +0400

    Add: responsive mobile menu to Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,46 @@
 import "./Navbar.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { IoMdClose } from "react-icons/io";
+import { IoMdMenu } from "react-icons/io";
 import logo from "../../assets/nav-logo.png";
 import NavLinks from "../../data/NavLinks";
 
 const Navbar = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleMenuClick = () => {
+    setIsClicked(!isClicked);
+  };
   return (
     <nav>
+      {/* Mobile */}
+      <div className={`navbar-slider ${isClicked ? "navbar-slider-open" : ""}`}>
+        {isClicked && (
+          <div>
+            <IoMdClose onClick={handleMenuClick} />
+            <ul>
+              {NavLinks.map((item) => {
+                return (
+                  <li key={item.link}>
+                    <Link to={item.link} onClick={handleMenuClick}>
+                      {item.title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="navbar-slider_btn">
+              <button href="/" className="navbar-btn-sign-in">
+                Sign In
+              </button>
+              <button href="/" className="navbar-btn-register">
+                Register
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* Desktop */}
       <div className="navbar">
         <div className="navbar-img">
           <Link to="/">
@@ -28,6 +63,9 @@ const Navbar = () => {
           <a href="/" className="navbar-btn-register">
             Register
           </a>
+        </div>
+        <div className="navbar-menu" onClick={handleMenuClick}>
+          <IoMdMenu />
         </div>
       </div>
     </nav>
