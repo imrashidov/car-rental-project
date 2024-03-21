84ff8b45bcb8cbddc5b0f3ed2497dd4c73f20190
imrashidov <farikrashidov@gmail.com>
2024-03-21 04:05:27 +0400

    Refactor Book component form layout

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -9,61 +9,63 @@ const Book = () => {
       <div className="container">
         <div className="book-content">
           <h1>Book a car</h1>
-          <form className="book-form">
-            <div className="book-form-select">
-              <label>
-                <FaCar /> Select Your Car Type <b>*</b>
-              </label>
-              <select>
-                <option>Select your type</option>
-                <option value="Audi A1 S-Line">Audi A1 S-Line</option>
-                <option value="VW Golf 6">VW Golf 6</option>
-                <option value="Toyota Camry">Toyota Camry</option>
-                <option value="BMW 320 ModernLine">BMW 320 ModernLine</option>
-                <option value="Mercedes-Benz GLK">Mercedes-Benz GLK</option>
-                <option value="VW Passat CC">VW Passat CC</option>
-              </select>
-            </div>
-            <div className="book-form-select">
-              <label>
-                <FaLocationDot /> Pick-up <b>*</b>
-              </label>
-              <select>
-                <option>Select pick-up location</option>
-                <option value="Baku">Baku</option>
-                <option value="Sumgait">Sumgait</option>
-                <option value="Khirdalan">Khirdalan</option>
-                <option value="Jabrayil">Jabrayil</option>
-                <option value="Shaki">Shaki</option>
-              </select>
-            </div>
-            <div className="book-form-select">
-              <label>
-                <FaLocationDot /> Drop-off <b>*</b>
-              </label>
-              <select>
-                <option>Select drop-off location</option>
-                <option value="Baku">Baku</option>
-                <option value="Sumgait">Sumgait</option>
-                <option value="Khirdalan">Khirdalan</option>
-                <option value="Jabrayil">Jabrayil</option>
-                <option value="Shaki">Shaki</option>
-              </select>
-            </div>
-            <div className="book-form-time">
-              <label>
-                <FaCalendarAlt /> Pick-up <b>*</b>
-              </label>
-              <input type="date" id="pickTime" />
-            </div>
-            <div className="book-form-time">
-              <label>
-                <FaCalendarAlt /> Drop-off <b>*</b>
-              </label>
-              <input type="date" id="dropTime" />
-            </div>
-            <button type="submit">Search</button>
-          </form>
+          <div className="book-form">
+            <form>
+              <div className="book-form-select">
+                <label>
+                  <FaCar /> Select Your Car Type <b>*</b>
+                </label>
+                <select>
+                  <option>Select your type</option>
+                  <option value="Audi A1 S-Line">Audi A1 S-Line</option>
+                  <option value="VW Golf 6">VW Golf 6</option>
+                  <option value="Toyota Camry">Toyota Camry</option>
+                  <option value="BMW 320 ModernLine">BMW 320 ModernLine</option>
+                  <option value="Mercedes-Benz GLK">Mercedes-Benz GLK</option>
+                  <option value="VW Passat CC">VW Passat CC</option>
+                </select>
+              </div>
+              <div className="book-form-select">
+                <label>
+                  <FaLocationDot /> Pick-up <b>*</b>
+                </label>
+                <select>
+                  <option>Select pick-up location</option>
+                  <option value="Baku">Baku</option>
+                  <option value="Sumgait">Sumgait</option>
+                  <option value="Khirdalan">Khirdalan</option>
+                  <option value="Jabrayil">Jabrayil</option>
+                  <option value="Shaki">Shaki</option>
+                </select>
+              </div>
+              <div className="book-form-select">
+                <label>
+                  <FaLocationDot /> Drop-off <b>*</b>
+                </label>
+                <select>
+                  <option>Select drop-off location</option>
+                  <option value="Baku">Baku</option>
+                  <option value="Sumgait">Sumgait</option>
+                  <option value="Khirdalan">Khirdalan</option>
+                  <option value="Jabrayil">Jabrayil</option>
+                  <option value="Shaki">Shaki</option>
+                </select>
+              </div>
+              <div className="book-form-time">
+                <label>
+                  <FaCalendarAlt /> Pick-up <b>*</b>
+                </label>
+                <input type="date" id="pickTime" />
+              </div>
+              <div className="book-form-time">
+                <label>
+                  <FaCalendarAlt /> Drop-off <b>*</b>
+                </label>
+                <input type="date" id="dropTime" />
+              </div>
+              <button type="submit">Search</button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
